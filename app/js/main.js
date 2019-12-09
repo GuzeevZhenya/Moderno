65bc23bcea850254d483f760faebb939bc419ba7
GuzeevZhenya <shyst.evgeny@mail.ru>
2019-12-09 16:39:06 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function() {
-    var mixer = mixitup('.products__inner-box')
+   
+    $(".rate-star").rateYo({
+        rating: 3,
+        starWidth: "12px",
+        readOnly:true,
+        multiColor: {
+            "startColor": "#FF0000", //RED
+            "endColor": "#00FF00" //GREEN
+        }
+        
+    });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows:false,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+    });
+    
+   var mixer = mixitup('.products__inner-box')
 })
